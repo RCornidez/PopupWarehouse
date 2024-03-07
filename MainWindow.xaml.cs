13137a7120a7d5d3c25003fac0e8fd5c8b5dccc7
Rodrigo Cornidez <rodrigo@cornidez.com>
2024-03-06 22:55:51 -0700

    Scaffold initial login window with placeholder UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,49 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PopupWarehouse;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace PopupWarehouse
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UsernameGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text == "Username")
+            {
+                Username.Text = "";
+                Username.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void UsernameLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Username.Text))
+            {
+                Username.Text = "Username";
+                Username.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void LoginClick(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = PasswordBox.Password; 
+
+        }
+        private bool AuthenticateUser(string username, string password)
+        {
+            return false;
+        }
+
+        private void OtherOptionsMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Other options clicked!");
+        }
+
     }
 }
